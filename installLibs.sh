9486d7aa117a77e3ca1193311b7d7907e89cea7a
Linux User <eric@localhost.wireless.reshall.calpoly.edu>
2015-05-22 01:19:53 +0000

    separated makeFS.sh from installLibs.sh

--- installLibs.sh
+++ installLibs.sh
@@ -3,35 +3,6 @@ VIRTUOS_HOME=$PWD/VirtuOS-master
 echo $VIRTUOS_HOME
 cd $VIRTUOS_HOME
 
-# Ensure copies made for /usr/sclib
-sudo mkdir -p /usr/sclib
-sudo mkdir -p /usr/sclib/dev
-sudo mkdir -p /usr/sclib/home
-sudo mkdir -p /usr/sclib/root
-sudo mkdir -p /usr/sclib/run
-sudo mkdir -p /usr/sclib/tmp
-sudo mkdir -p /usr/sclib/media
-sudo mkdir -p /usr/sclib/sys
-sudo mkdir -p /usr/sclib/proc
-sudo mkdir -p /usr/sclib/var
-sudo mkdir -p /usr/sclib/usr
-sudo cp -r /bin /usr/sclib/bin
-sudo cp -r /lib /usr/sclib/lib
-sudo cp -r /etc /usr/sclib/etc
-sudo cp -r /sbin /usr/sclib/sbin
-sudo cp -r /usr/bin /usr/sclib/usr
-sudo cp -r /usr/etc /usr/sclib/usr
-sudo cp -r /usr/include /usr/sclib/usr
-sudo cp -r /usr/info /usr/sclib/usr
-sudo cp -r /usr/lib /usr/sclib/usr
-sudo cp -r /usr/local /usr/sclib/usr
-sudo cp -r /usr/man /usr/sclib/usr
-sudo cp -r /usr/sbin /usr/sclib/usr
-sudo cp -r /usr/share /usr/sclib/usr
-sudo cp -r /usr/src /usr/sclib/usr
-sudo cp -r /usr/var /usr/sclib/usr
-sudo cp -r /usr/x86_64-alpine-linux-uclibc /usr/sclib/usr
-
 #####################################################################
 # Installing the uclib
 echo "[VirtuOS] - Compiling and Installing Patched uclib"
