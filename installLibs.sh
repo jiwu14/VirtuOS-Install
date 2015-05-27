5593a2c9d2ae4a27df195a3a7ebda3b0b4825408
Sam Wu <sjiwu14@gmail.com>
2015-05-27 15:58:18 -0700

    Added make clean to building libs in case you change configuration and rerun installLib.

--- installLibs.sh
+++ installLibs.sh
@@ -7,16 +7,16 @@ cd $VIRTUOS_HOME
 # Installing the uclib
 echo "[VirtuOS] - Compiling and Installing Patched uclib"
 cd source/virtuos-uclibc
-make menuconfig; make; sudo make PREFIX=/usr/sclib install
+make menuconfig; make clean; make; sudo make PREFIX=/usr/sclib install
 cd $VIRTUOS_HOME
 #####################################################################
 # Installing libaio
 echo "[VirtuOS] - Compiling and Installing Patched libaio"
 cd source/virtuos-libaio
-make; sudo make PREFIX=/usr/sclib/usr install
+make clean; make; sudo make PREFIX=/usr/sclib/usr install
 cd $VIRTUOS_HOME
 #####################################################################
 # Installing Syscall utilities
 echo "[VirtuOS] - Compiling and Installing System Call Utilities"
-cd source/syscall-utils; make; sudo make install; cd $VIRTUOS_HOME
+cd source/syscall-utils; make clean; make; sudo make install; cd $VIRTUOS_HOME
 #####################################################################
